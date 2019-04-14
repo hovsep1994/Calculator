cc713317145d14567e23e77f6988751329ef0186
Hovsep <hovsep1994@gmail.com>
2019-04-14 17:13:52 +0400

    Add calculator homework6

--- src/com/aca/calculator/Calculator6.java
+++ src/com/aca/calculator/Calculator6.java
@@ -7,23 +7,44 @@ public class Calculator6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please type the expression");
-        //( ( 3 + 4 ) * 2 ) - 1
 //        String expression = scanner.nextLine();
-        String expression = "( ( 3 + 4 + 3 ) * 2 ) - 1";
+        String expression = "( ( 3 + 4 * 3 ) * 2 ) - ( -1 + 3 * 3 )";
         String result = calculateExpression(expression);
         System.out.println(expression + " = " + result);
     }
 
     private static String calculateExpression(String expression) {
         while (!isNumber(expression)) {
+            System.out.println(expression);
             String mostPrioritizedSubExpression = findMostPrioritizedSubExpression(expression);
+            if (isNumber(mostPrioritizedSubExpression)) {
+                expression = expression.replace("( " + mostPrioritizedSubExpression + " )", mostPrioritizedSubExpression);
+                continue;
+            }
             String subExpressionResult = calculate(mostPrioritizedSubExpression);
             expression = expression.replace(mostPrioritizedSubExpression, subExpressionResult);
         }
         return expression;
     }
 
-    static String findMostPrioritizedSubExpression(String expression) {
+    private static String findMostPrioritizedSubExpression(String expression) {
+        String result = getExpressionInBrackets(expression);
+        if (isNumber(result)) {
+            return result;
+        }
+        String[] split = result.split(" ");
+        if (result.contains("*") || result.contains("/")) {
+            for (int i = 0; i < split.length; i++) {
+                if ("*/".contains(split[i])) {
+                    return split[i - 1] + " " + split[i] + " " + split[i + 1];
+                }
+            }
+        }
+        return split[0] + " " + split[1] + " " + split[2];
+    }
+
+    private static String getExpressionInBrackets(String expression) {
+        String result = "";
         if (expression.contains("(")) {
             String[] split = expression.split(" ");
             int openingBracketIndex = -1;
@@ -36,34 +57,25 @@ public class Calculator6 {
                     break;
                 }
             }
-            String result = "";
-            for (int i = openingBracketIndex; i < closingBracketIndex + 1; i++) {
+            for (int i = openingBracketIndex + 1; i < closingBracketIndex; i++) {
                 result += split[i];
-                if (i != closingBracketIndex) {
+                if (i != closingBracketIndex - 1) {
                     result += " ";
                 }
             }
             return result;
         }
-        String[] split = expression.split(" ");
-        if (expression.contains("*") || expression.contains("/")) {
-            for (int i = 0; i < split.length; i++) {
-                if ("*/".contains(split[i])) {
-                    return split[i - 1] + " " + split[i] + " " + split[i + 1];
-                }
-            }
-        }
-        return split[0] + " " + split[1] + " " + split[2];
+        return expression;
     }
 
-    static String calculate(String expression) {
+    private static String calculate(String expression) {
         expression = expression.replace("( ", "");
         expression = expression.replace(" )", "");
         String[] array = expression.split(" ");
         return "" + calculate(Double.parseDouble(array[0]), array[1], Double.parseDouble(array[2]));
     }
 
-    static double calculate(double a, String operation, double b) {
+    private static double calculate(double a, String operation, double b) {
         switch (operation) {
             case "*":
                 return a * b;
@@ -78,7 +90,7 @@ public class Calculator6 {
         }
     }
 
-    static boolean isNumber(String expression) {
+    private static boolean isNumber(String expression) {
         // Or you can do like this
         // return str.matches("-?\\d+(\\.\\d+)?");
         try {
@@ -87,17 +99,6 @@ public class Calculator6 {
         } catch (NumberFormatException e) {
             return false;
         }
-    }
-
-    static String concatWithSpace(String... strings) {
-        String result = "";
-        for (int i = 0; i < strings.length; i++) {
-            result += strings[i];
-            if (i != strings.length - 1) {
-                result += " ";
-            }
-        }
-        return result;
     }
 
 }
